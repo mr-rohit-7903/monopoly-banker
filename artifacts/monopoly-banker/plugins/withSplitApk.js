56cd5475c0db46be792743a4d6fe7f3c802c3e4a
Rohit <rohit098bej@gmail.com>
2026-07-05 13:51:01 +0530

    fix: correctly inject ABI splits for modern RN versions

--- artifacts/monopoly-banker/plugins/withSplitApk.js
+++ artifacts/monopoly-banker/plugins/withSplitApk.js
@@ -7,6 +7,13 @@ module.exports = function withSplitApk(config) {
         /def enableSeparateBuildPerCPUArchitecture = false/,
         'def enableSeparateBuildPerCPUArchitecture = true'
       );
+    } else {
+      if (!config.modResults.contents.includes('splits {')) {
+        config.modResults.contents = config.modResults.contents.replace(
+          /android\s*\{/,
+          `android {\n    splits {\n        abi {\n            reset()\n            enable true\n            universalApk false\n            include "armeabi-v7a", "arm64-v8a", "x86", "x86_64"\n        }\n    }`
+        );
+      }
     }
     return config;
   });
